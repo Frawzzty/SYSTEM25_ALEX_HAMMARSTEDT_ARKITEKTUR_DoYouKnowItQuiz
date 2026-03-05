4b960b5e606c6ef048290728bfdb3b0309f32af0
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2026-03-05 18:56:02 +0100

    Regeister Routing on QuizBuilder Pages

--- DoYouKnowIt.Presentation/AppShell.xaml.cs
+++ DoYouKnowIt.Presentation/AppShell.xaml.cs
@@ -5,8 +5,18 @@
         public AppShell()
         {
             InitializeComponent();
+
+            //TEST PAGES
             Routing.RegisterRoute(nameof(Views.Test.QueryPropTestPage), typeof(Views.Test.QueryPropTestPage));
+
+            //QUIZ BUILDER
             Routing.RegisterRoute(nameof(Views.QB.QBSelectPage), typeof(Views.QB.QBSelectPage));
+            Routing.RegisterRoute(nameof(Views.QB.QBEditQuizPage), typeof(Views.QB.QBEditQuizPage));
+            Routing.RegisterRoute(nameof(Views.QB.QBEditQuestionPage), typeof(Views.QB.QBEditQuestionPage));
+            Routing.RegisterRoute(nameof(Views.QB.QBEditAnswerPage), typeof(Views.QB.QBEditAnswerPage));
+
+            //QUIZ PLAY
+
         }
     }
 }
