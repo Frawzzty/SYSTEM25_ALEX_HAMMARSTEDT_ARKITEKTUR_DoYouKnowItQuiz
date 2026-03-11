a96eaf760b85f4fb6e7a9848e6efabf570d4344d
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2026-03-11 19:24:42 +0100

    Mainpage Login viewmodel

--- DoYouKnowIt.Presentation/ViewModels/MainPageViewModel.cs
+++ DoYouKnowIt.Presentation/ViewModels/MainPageViewModel.cs
@@ -40,5 +40,10 @@ namespace DoYouKnowIt.Presentation.ViewModels
         public ICommand GoQuizBuilderPageCommand { get; set; }
         public ICommand GoCountryFlagLookupPageCommand { get; set; }
         #endregion
+
+        private string _username;
+        private string _password;
+        public string Username { get { return _username; } set { _username = value; OnPropertyChanged(nameof(Username)); } }
+        public string Password { get { return _password; } set { _password = value; OnPropertyChanged(nameof(Password)); } }
     }
 }
