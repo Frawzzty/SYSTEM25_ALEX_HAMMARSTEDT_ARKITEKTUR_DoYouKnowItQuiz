5d59c55b9601619f3cac18419c77eff9a01a0e5a
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2026-03-14 10:50:15 +0100

    login better

--- DoYouKnowIt.Application/Facades/ILoginFacade.cs
+++ DoYouKnowIt.Application/Facades/ILoginFacade.cs
@@ -9,6 +9,7 @@ namespace DoYouKnowIt.Application.Facades
     public interface ILoginFacade
     {
         Task<bool> UserIsAdminAsync();
+        Task<bool> UserIsLoggedIn();
         public Task<bool> UserLogin(string username, string password);
         public void UserLogout();
     }
