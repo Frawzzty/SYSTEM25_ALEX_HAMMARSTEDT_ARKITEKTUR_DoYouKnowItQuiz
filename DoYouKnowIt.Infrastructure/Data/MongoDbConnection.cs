f71db3a5c3701a658b5d4946c389528ec9210ca7
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2026-03-15 10:48:03 +0100

    MongoDbContext

---
+++ DoYouKnowIt.Infrastructure/Data/MongoDbConnection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoYouKnowIt.Infrastructure.Data
+{
+    public class MongoDbConnection
+    {
+        public MongoClient GetClient()
+        {
+            var connStr = "mongodb+srv://alexh_db_user:JHp0b6l1LcwnCQzF@cluster0.qwoupuh.mongodb.net/?appName=Cluster0"; // :D //Super secret
+            
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connStr));
+            MongoClient client = new MongoClient(settings);
+
+            return client;
+        }
+    }
+}
